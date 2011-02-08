15f58f6e2fb1af366af79f5c4b85dd2dd35a6290
Ryan <ryan@oroboto.com>
2011-02-08 10:16:23 +0000

    [CHG] renamed /includes/ to /src/

--- styles/css.php
+++ styles/css.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-require_once('../includes/CSSUnity.class.php');
+require_once('../src/CSSUnity.class.php');
 $cssunity = new CSSUnity($_GET['s']);
 //echo $cssunity->combine_files();
 //echo $cssunity->encode_resources();
