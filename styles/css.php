2c971003a773918e4e19ba0edee8d9e5d0bdedad
Ryan <ryan@oroboto.com>
2011-03-02 03:01:49 +0000

    [CHG] improved stylesheet handler with usage comments

--- styles/css.php
+++ styles/css.php
@@ -1,9 +1,26 @@
 <?php
+// fail on error
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+// set response header
+header('Content-type: text/css');
+
+// include class
 require_once('../src/CSSUnity.class.php');
+
+// instantiate object with comma-separated list of file paths from query string
 $cssunity = new CSSUnity($_GET['s']);
+
+// other public functions can be used, for simple combination or normalization
 //echo $cssunity->combine_files();
-//echo $cssunity->encode_resources();
-$cssunity->unify(isset($_GET['type']) ? $_GET['type'] : false, isset($_GET['separate']));
+//echo $cssunity->normalize();
+
+// parse stylesheets according to options specified in query string
+// examples:
+//     type=none
+//     type=datauri
+//     type=mhtml&separate
+// default arguments (false, false) are used if no options are specified
+echo $cssunity->parse(isset($_GET['type']) ? $_GET['type'] : false, isset($_GET['separate']));
 ?>
