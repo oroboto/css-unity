50f9179144c864f525622bdb49cc513bdaf6d475
Ryan <ryan@oroboto.com>
2011-04-21 15:40:16 -0700

    [ADD] github links, example mentions fallback for datauri option

--- datauri.php
+++ datauri.php
@@ -27,7 +27,7 @@
             </li>
             <li>
                 <strong>Data URI</strong>
-                <p>Only data URIs are written. IE8+/Moz/Webkit/Opera only.</p>
+                <p>Only data URIs are written. IE8+/Moz/Webkit/Opera only (IE6/7 will use original image).</p>
             </li>
             <li>
                 <a href="./mhtml">MHTML</a>
@@ -65,5 +65,6 @@
             </ul>
         </section>
     </div>
+    <a href="https://github.com/oroboto/css-unity"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://d3nwyuy0nl342s.cloudfront.net/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>
 </body>
 </html>
